ae610b6fc13f36aabb4cf2e67e437a11c18e69e6
adriaanbalt <adriaan@balt.us>
2014-02-10 18:27:56 -0500

    adding details and talent sub pages

---
+++ details.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>wanderlust management</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=640,user-scalable=no">
+
+		<link rel="stylesheet" href="assets/css/screen.css">
+
+		<script src="assets/js/lib/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+	</head>
+	<body>
+		<!--[if lt IE 7]>
+			<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+		<![endif]-->
+
+		
+		<div id="content" class='pink'>
+			<div id='navigation' class='desktop'>
+				<div class='logo'>
+					<img src='assets/img/logo-pink.svg'/>
+				</div>
+				<nav>
+					<a href='talent'>TALENT</a>
+					<a href='about'>ABOUT</a>
+					<a href='contact'>JOIN</a>
+					<a href='guidance'>GUIDANCE</a>
+				</nav>
+			</div>
+			<section id='intro' class='hidden'>
+				<div class='valign'></div>
+				<div class='vcontent'>
+					<img src='assets/img/logo-pink.svg'/>
+				</div>
+			</section>
+			<section id='details'>
+				<div>
+					<img src='assets/img/talent/chelsea/0-cover.jpg'/>
+					<h2>CHELSEA</h2>
+				</div>
+			</section>
+		</div>
+
+		<script src="assets/js/lib/jquery-1.8.3.js"></script>
+		<script src="assets/js/lib/jquery.history.js"></script>
+		<script src="assets/js/lib/requestAnimationFrame.js"></script>
+		<script src="assets/js/wanderlust.models.js"></script>
+		<script src="assets/js/wanderlust.js"></script>
+
+		<script>
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-###-1']);
+			_gaq.push(['_trackPageview']);
+			(function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+		</script>
+	</body>
+</html>
+
+
