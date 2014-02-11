7bd4c150931e65d9ab06bf3c68d5fd01a5c9aa3f
adriaanbalt <adriaan@balt.us>
2014-02-11 12:46:56 -0500

    removing the COLORS helper funtion

--- bootstrap/dependencies.php
+++ bootstrap/dependencies.php
@@ -1,12 +1,5 @@
 <?php
 
-App::singleton('Colors', function()
-{
-	$_color = new ColorModel;
-	$colors = $_color->getAll();
-	return $colors;
-});
-
 function debug ( $arg ) {
 	file_put_contents( '/Users/adriaan.scholvinck/PROJECTS/BALT/ShoppingCart/logs/temp.log', $arg, FILE_APPEND );
 
