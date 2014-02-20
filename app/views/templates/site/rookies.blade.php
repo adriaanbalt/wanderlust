9f747495a03ffb371f4250aa2890dc68b2cb7e5d
adriaanbalt <adriaan@balt.us>
2014-02-20 14:31:14 -0500

    adding the beginning of the rookies controler and template and model

---
+++ app/views/templates/site/rookies.blade.php
@@ -0,0 +1,13 @@
+@section('content')
+<h1>rookies</h1>
+	<ul>
+@foreach( $data as $item )
+		<li>
+			<a href="some-girls/{{ $item['slug'] }}">
+				<img src="{{ URL::asset( $item->cover->path ) }}"/>
+				<p>{{ $item['name'] }}</p>
+			</a>
+		</li>
+@endforeach
+	</ul>
+@stop
