e60f7b567f851e0618cf8eee6df02a77104bd194
adriaanbalt <adriaan@balt.us>
2014-02-20 14:32:09 -0500

    logo is now black on the header template

--- app/views/partials/header.blade.php
+++ app/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header>
 	<div class='wrapper'>
-		<div class='logo-pink'>
-			<img src="images/logo-pink.svg">
+		<div class='logo-black'>
+			<img src="images/logo-black.svg">
 		</div>
 		@include('partials.navigation')
 	</div>
