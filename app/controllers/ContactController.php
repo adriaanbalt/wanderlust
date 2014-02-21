00ac762df51f85307b27415b1b46f65607fa3a75
adriaanbalt <adriaan@balt.us>
2014-02-21 14:54:10 -0500

    contact controller loads up the contactmodel data

--- app/controllers/ContactController.php
+++ app/controllers/ContactController.php
@@ -4,7 +4,7 @@ class ContactController extends BaseController {
 
 	public function index()
 	{
-		$data = RookiesModel::all();
+		$data = ContactModel::all();
 		$this->layout->content = View::make('templates.site.contact')->withData($data);
 	}
 
