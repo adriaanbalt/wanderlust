ac0f607b0bd4ad416cd5d77a9e496ce0c2616e53
adriaanbalt <adriaan@balt.us>
2014-02-21 16:19:06 -0500

    seed has black and white photos

--- app/database/seeds/PhotosTableSeeder.php
+++ app/database/seeds/PhotosTableSeeder.php
@@ -11,17 +11,17 @@ class PhotosTableSeeder extends Seeder {
 			array(
 					'title' => 'photo 1',
 					'slug' => 'cover',
-					'path' => 'images/models/chelsea/0-cover.jpg'
+					'path' => 'images/models/chelsea/0-cover-bw.jpg'
 			),
 			array(
 					'title' => 'photo 2',
 					'slug' => 'cover',
-					'path' => 'images/models/irina/0-cover.jpg'
+					'path' => 'images/models/irina/0-cover-bw.jpg'
 			),
 			array(
 					'title' => 'photo 3',
 					'slug' => 'cover',
-					'path' => 'images/models/maria/0-cover.jpg'
+					'path' => 'images/models/maria/0-cover-bw.jpg'
 			)
 		);
 
