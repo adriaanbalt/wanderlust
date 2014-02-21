38c4c8ef0c45f61424845258f4b649b1de41a76d
adriaanbalt <adriaan@balt.us>
2014-02-21 14:56:14 -0500

    about tmplate has no real info but exists

--- app/views/templates/site/about.blade.php
+++ app/views/templates/site/about.blade.php
@@ -1,3 +1,11 @@
 @section('content')
 <h1>about</h1>
+@foreach( $data as $item )
+	<li>
+		<a href="rookies/{{ $item['slug'] }}">
+			<img src="{{ URL::asset( $item->cover->path ) }}"/>
+			<p>{{ $item['name'] }}</p>
+		</a>
+	</li>
+@endforeach
 @stop
