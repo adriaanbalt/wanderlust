fcc9cb97757600e49ce58a930404f9a5cf200c04
adriaanbalt <adriaan@balt.us>
2014-02-21 14:53:41 -0500

    basic contact template pulilng infrormation from the contactmodel

--- app/views/templates/site/contact.blade.php
+++ app/views/templates/site/contact.blade.php
@@ -1,3 +1,17 @@
 @section('content')
 <h1>contact</h1>
+@foreach( $data as $item )
+<div>
+	<p class='address'>{{ $item->address }}</p>
+	<p class='telephone'>
+		<a href='tel:{{ $item->telephone }}'>{{ $item->telephone }}</a>
+	</p>
+	<p class='fax'>
+		<a href='tel:{{ $item->telephone }}'>{{ $item->fax }}</a>
+	</p>
+	<p class='email'>
+		<a href='mailto:{{ $item->email }}'>{{ $item->email }}</a>
+	</p>
+</div>
+@endforeach
 @stop
