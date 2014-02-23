efb2668b0b91d35469cf931b80dca6e7b97db890
adriaanbalt <adriaan@balt.us>
2014-02-23 01:09:29 -0500

    talent sub route

--- app/routes.php
+++ app/routes.php
@@ -2,7 +2,9 @@
 
 Route::get('/', 'HomeController@index');
 Route::get('/some-girls', 'SomegirlsController@index');
+Route::get('/some-girls/{id}', 'SomegirlsController@details');
 Route::get('/rookies', 'RookiesController@index');
+Route::get('/rookies/{id}', 'RookiesController@details');
 Route::get('/backstage-pass', 'AboutController@index');
 Route::get('/contact', 'ContactController@index');
 Route::get('/join-the-band', 'JoinController@index');
