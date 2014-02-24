f1ee572d48c712530e792302f8adc575aa5baf2e
adriaanbalt <adriaan@balt.us>
2014-02-24 17:55:04 -0500

    routes look for new file name like WOMEN and DEVELOPMENT

--- app/routes.php
+++ app/routes.php
@@ -1,10 +1,10 @@
 <?php
 
 Route::get('/', 'HomeController@index');
-Route::get('/some-girls', 'SomegirlsController@index');
-Route::get('/some-girls/{id}', 'SomegirlsController@details');
-Route::get('/rookies', 'RookiesController@index');
-Route::get('/rookies/{id}', 'RookiesController@details');
+Route::get('/some-girls', 'WomenController@index');
+Route::get('/some-girls/{id}', 'WomenController@details');
+Route::get('/rookies', 'DevelopmentController@index');
+Route::get('/rookies/{id}', 'DevelopmentController@details');
 Route::get('/backstage-pass', 'AboutController@index');
 Route::get('/contact', 'ContactController@index');
 Route::get('/join-the-band', 'JoinController@index');
