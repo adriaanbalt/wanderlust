353b016aa6e587be04eabd67d993744a066c7a15
adriaanbalt <adriaan@balt.us>
2014-02-28 15:38:18 -0500

    removing models from the index page since its just the intro

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -4,9 +4,6 @@ class HomeController extends BaseController {
 
 	public function index()
 	{
-		
-		$data = Women::all();
-		$this->layout->content = View::make('templates.site.models')->withData($data);
 	}
 
 }
