9a2bccb4cc2d2a1582543843148e362ab323b103
adriaanbalt <adriaan@balt.us>
2014-03-02 13:56:46 -0500

    injecting model stats into details view via controller

--- app/controllers/WomenController.php
+++ app/controllers/WomenController.php
@@ -12,9 +12,18 @@ class WomenController extends BaseController {
 
 	public function details( $id )
 	{
-		$data = Women::where( 'slug', '=', $id )->firstOrFail()->photos;
+		$photos = Women::where( 'slug', '=', $id )->firstOrFail()->photos;
+		
 		$name = Women::where( 'slug', '=', $id )->firstOrFail()->name;
-		$this->layout->content = View::make('templates.site.details')->with( 'data', $data)->with('name', $name );
+		$height = Women::where( 'slug', '=', $id )->firstOrFail()->height;
+		$bust = Women::where( 'slug', '=', $id )->firstOrFail()->bust;
+		$hips = Women::where( 'slug', '=', $id )->firstOrFail()->hips;
+		$waist = Women::where( 'slug', '=', $id )->firstOrFail()->waist;
+		$eyes = Women::where( 'slug', '=', $id )->firstOrFail()->eyes;
+		$hair = Women::where( 'slug', '=', $id )->firstOrFail()->hair;
+		$shoes = Women::where( 'slug', '=', $id )->firstOrFail()->shoes;
+
+		$this->layout->content = View::make('templates.site.details')->with( 'photos', $photos)->with('name', $name )->with('height', $height )->with('bust', $bust )->with('hips', $hips )->with('waist', $waist )->with('eyes', $eyes )->with('hair', $hair )->with('shoes', $shoes );
 	}
 
 }
