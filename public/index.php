86367f2318a1d40fd6fac4902c1bea089f54da94
adriaanbalt <adriaan@balt.us>
2014-08-12 10:46:18 -0400

    fixed paths in the index.php

--- public/index.php
+++ public/index.php
@@ -18,7 +18,7 @@
 |
 */
 
-require '/home/balt/laravel-wanderlust/bootstrap/autoload.php';
+require __DIR__.'/../bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -32,7 +32,7 @@ require '/home/balt/laravel-wanderlust/bootstrap/autoload.php';
 |
 */
 
-$app = require_once '/home/balt/laravel-wanderlust/bootstrap/start.php';
+$app = require_once __DIR__.'/../bootstrap/start.php';
 
 /*
 |--------------------------------------------------------------------------
